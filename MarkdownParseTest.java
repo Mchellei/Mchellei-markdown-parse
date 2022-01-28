4fb54d668463cf9aa7d5b7b426f28248e0ea03f1
Mchellei <97761873+Mchellei@users.noreply.github.com>
2022-01-27 22:16:25 -0500

    Added Test 1

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -14,9 +14,11 @@ public class MarkdownParseTest {
 
     @Test
     public void testFile1() throws IOException {
-        String contents= Files.readString(Path.of("./test-file.md"));
+        String contents= Files.readString(Path.of("/Users/michellechen/Documents/GitHub/markdown-parse/test-file.md"));
         List<String> expect = List.of("https://something.com", "some-page.html");
         assertEquals(MarkdownParse.getLinks(contents), expect);
     }
+
+   
     
 }
--- test-file2.md
+++ test-file2.md
@@ -3,4 +3,4 @@
 [a link!](https://test2.com)
 [another link!](test2.html)
 
-some paragraph text after the links
+texxxxxttttttssss
